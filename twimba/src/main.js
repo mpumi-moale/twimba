fa489de1b99f06b4cd5580cbedd3af05dcc9ebb6
Mpumelelo Moale <moalempumi5@gmail.com>
2026-06-12 22:04:06 +0200

    getfeedhtml

--- twimba/src/main.js
+++ twimba/src/main.js
@@ -1,3 +1,40 @@
 import { tweetsData } from "./data";
 
-console.log(tweetsData)
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function() {
+  console.log(tweetInput.value)
+})
+
+function getFeedHtml() {
+  let feedHtml = ``
+
+  for(let tweetData of tweetsData) {
+    feedHtml += `
+            <div class="tweet">
+              <div class="tweet-inner">
+                <img src="${tweetData.profilePic}" class="profile-pic">
+                  <div>
+                      <p class="handle">${tweetData.handle}</p>
+                      <p class="tweet-text">${tweetData.tweetText}}</p>
+                      <div class="tweet-details">
+                          <span class="tweet-detail">
+                              ${tweetData.replies.length}
+                          </span>
+                          <span class="tweet-detail">
+                              ${tweetData.likes}
+                          </span>
+                          <span class="tweet-detail">
+                              ${tweetData.retweets}
+                          </span>
+                      </div>   
+                  </div>            
+              </div>
+          </div>
+        `
+  }
+  console.log(feedHtml)
+}
+
+getFeedHtml()
