b9a4fe62dee821f306120f248da6d690270fcab4
Mpumelelo Moale <moalempumi5@gmail.com>
2026-06-19 11:38:07 +0200

    accessing any clicked item using data attributes and dataset

--- twimba/README.md
+++ twimba/README.md
@@ -1,1 +1,6 @@
 "# twimba, twitter or x clone"
+
+
+# Lessons
+
+# data attributes, example : e.target.dataset.heart
--- twimba/src/main.js
+++ twimba/src/main.js
@@ -7,6 +7,10 @@ tweetBtn.addEventListener('click', function() {
   console.log(tweetInput.value)
 })
 
+document.addEventListener('click',function(e) {
+  console.log(e.target.dataset.heart)
+})
+
 function getFeedHtml() {
   let feedHtml = ``
 
