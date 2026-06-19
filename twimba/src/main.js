f66378fa56453db9ba3d2a103c9fbe8065c5d2d0
Mpumelelo Moale <moalempumi5@gmail.com>
2026-06-19 15:32:35 +0200

    refactored the if else toggle like condition using the (not) ! operator

--- twimba/src/main.js
+++ twimba/src/main.js
@@ -23,12 +23,11 @@ function handleLikeClick(tweetId) {
 
   if(targetTweetObj.isLiked) {
     targetTweetObj.likes--
-    targetTweetObj.isLiked = false
   }
   else {
     targetTweetObj.likes++
-    targetTweetObj.isLiked = true
   }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked
  
   // increaments likes
  
